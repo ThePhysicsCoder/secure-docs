117aa08f0fc258f3c0076a1963109535207bc33f
dineshkumar <dineshkumark94874@gmail.com>
2026-01-20 22:22:40 +0530

    watha.. this should be my final commit da

--- digitallocker/settings.py
+++ digitallocker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))gse7)-m*^@&-cyq7@^^rls@*$)^$pm-+wh+d5ol7gu!#8og-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

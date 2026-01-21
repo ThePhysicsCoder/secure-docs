0a39221442987045282d786d1ff8d83ef52db5c0
dineshkumar <dineshkumark94874@gmail.com>
2026-01-21 23:02:26 +0530

    might be last commit after removing reset password option

--- digitallocker/settings.py
+++ digitallocker/settings.py
@@ -127,8 +127,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
